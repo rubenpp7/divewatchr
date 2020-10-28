bced6aae01835594e775a86ce6f822f7eea23060
Ruben Perez <rubenp@vliz.be>
2020-10-28 18:19:06 +0100

    into package test

--- R/load_data.R
+++ R/load_data.R
@@ -1,3 +1,13 @@
+#' Loads logbook data into the R session
+#'
+#' This function reads and updates data files from the database.
+#' @param data The URL ID of the database in google spreadhsets.
+#' @return Reads one R list and the 2 dataframes within it into the R environment.
+#' @export
+#' @examples
+#' load_data("1qO7_0K1R-4i_MSgtT3zAYbZfxBmPMgQWAi7OmWmb1-M")
+
+
 ## library(tidyverse)
 ## library(leaflet) # for working with maps
 ## library(htmltools) # for working with html labels
@@ -46,13 +56,14 @@ scuba_clean <- scuba_log %>% left_join(scuba_geo, by = "locationID") %>%
                              filter (!is.na(decimalLatitude) & !is.na(decimalLatitude))
 
 # Some columns need a special data type. eventDate needs to be character to be good as a label
-scuba_clean <- scuba_clean %>%
+scuba_clean <<- scuba_clean %>%
                   mutate(decimalLatitude = as.numeric(decimalLatitude),
                          decimalLongitude = as.numeric(decimalLongitude),
                          eventDate = as.character(eventDate),
                          maximumDepthInMeters = as.character(maximumDepthInMeters),
                          bottomTime = as.character(bottomTime) ) %>%
-                  select(-eventTime)
+                  select(-eventTime) %>% 
+                  arrange(eventDate)
 
 # Remove NA's introduced by coercion (actually we should fix them in the original table)
 # scuba_clean <- scuba_clean %>% filter (!is.na(decimalLatitude) & !is.na(decimalLatitude))
@@ -69,7 +80,8 @@ scuba_map <-
            coords = c("decimalLongitude", "decimalLatitude"),
            crs = 4326)
 # Add coordinates as two extra columns
-  scuba_map <- scuba_map %>% bind_cols(coords_scuba)
+  scuba_map <<- scuba_map %>% bind_cols(coords_scuba) %>% 
+                              arrange(eventDate)
 
 # Double check the Coordinate Reference System (CRS)
 print(st_crs(scuba_map))
@@ -82,10 +94,9 @@ return(x)
 
 scuba_data <- load_data(logbook)
 
-# Separate list into objects
-scuba_map <- scuba_data$scuba_map %>% arrange(eventDate)
-scuba_clean <- scuba_data$scuba_clean %>% arrange(eventDate)
-
+# Create data tables as objects so I don't have to run the function everytime (needs R package)
+# usethis::use_data(scuba_map, overwrite = TRUE)
+# usethis::use_data(scuba_clean, overwrite = TRUE)
 
 
 #.........................................................
