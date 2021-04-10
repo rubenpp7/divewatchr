b7ac867eb0dd8d67c666098d4ace2dcfe0b100ec
Ruben Perez Perez <ruben.perper@gmail.com>
2021-04-10 20:42:36 +0200

    cache credentials

--- R/prep_data.R
+++ R/prep_data.R
@@ -38,7 +38,7 @@ prep_data <- function (data = NA,
 
 if(!is.na(data)){  
 # From Google sheets
-gs4_auth(email)
+gs4_auth(email, cache = ".secrets")
 (scuba_log <- read_sheet(data, sheet = "logbook", na = "")) 
 (scuba_geo <- read_sheet(data, sheet = "coordinates", na = ""))
 (scuba_typ <- read_sheet(data, sheet = "divetypes", na = ""))
